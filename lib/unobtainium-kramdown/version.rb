65382dd3ce95a980377a9241abcb504c3d86ee3c
Jens Finkhaeuser <jens@finkhaeuser.de>
2018-04-09 13:39:44 +0200

    Bump own version

--- lib/unobtainium-kramdown/version.rb
+++ lib/unobtainium-kramdown/version.rb
@@ -3,12 +3,12 @@
 # unobtainium-kramdown
 # https://github.com/jfinkhaeuser/unobtainium-kramdown
 #
-# Copyright (c) 2016 Jens Finkhaeuser and other unobtainium-kramdown contributors.
+# Copyright (c) 2016-2018 Jens Finkhaeuser and other unobtainium-kramdown contributors.
 # All rights reserved.
 #
 module Unobtainium
   module Kramdown
     # The current release version
-    VERSION = "0.1.0".freeze
+    VERSION = "0.2.0".freeze
   end # module Kramdown
 end # module Unobtainium
